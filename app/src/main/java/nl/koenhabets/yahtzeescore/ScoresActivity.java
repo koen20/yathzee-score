a8c6419ee498dfecda74111f4d9cbf772416721e
Koen Habets <koenhabets1@gmail.com>
2020-02-05 22:24:02 +0100

    Display score of player below nearby scores

--- app/src/main/java/nl/koenhabets/yahtzeescore/ScoresActivity.java
+++ app/src/main/java/nl/koenhabets/yahtzeescore/ScoresActivity.java
@@ -124,7 +124,7 @@ public class ScoresActivity extends AppCompatActivity {
                     }
                 });
                 builder.show();
-                return false;
+                return true;
             }
         });
 
