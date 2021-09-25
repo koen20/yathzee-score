f53147d9fa6439a066c566eec5e8615fb5ba4f6c
Koen Habets <koenhabets1@gmail.com>
2021-09-25 20:39:16 +0200

    Improve end game dialog

--- app/src/main/java/nl/koenhabets/yahtzeescore/dialog/GameEndDialog.kt
+++ app/src/main/java/nl/koenhabets/yahtzeescore/dialog/GameEndDialog.kt
@@ -24,6 +24,7 @@ class GameEndDialog(context: Context) {
         val tVGames = view2.findViewById<TextView>(R.id.textViewGamesPlayed)
         val tVAverageChange = view2.findViewById<TextView>(R.id.textViewAverageChange)
         val tvTotalToday = view2.findViewById<TextView>(R.id.textViewTotalToday)
+        val tvAvgDifToday = view2.findViewById<TextView>(R.id.textViewDifToday)
 
         scoreItems = DataManager.loadScores(context)
 
@@ -37,26 +38,30 @@ class GameEndDialog(context: Context) {
         calendar.set(Calendar.MINUTE, 59)
         val endTime = calendar.timeInMillis
         var dailyTotal = score
+        var gamesToday = 0
         for (i in scoreItems.indices) {
             val scoreItem = scoreItems[i]
             total += scoreItem.score
             count++
             if (scoreItem.date in startTime until endTime) {
                 dailyTotal += scoreItem.score
+                gamesToday += 1
             }
         }
-        val average1 = Math.round((total / count * 100.0) / 100.0)
-        val average2 = Math.round(((total + score) / (count + 1) * 100.0) / 100.0)
-        val dif = average2 - average1
+        val average1 = Math.round(total / count * 100.0) / 100.0
+        val average2 = Math.round((total + score) / (count + 1) * 100.0) / 100.0
+        val dif = Math.round((average2 - average1) * 100) / 100.0
+        val difToday = Math.round((average2 - ((total - dailyTotal + score) / (count - gamesToday))) * 100) / 100.0
 
-        tVGames.text = context.getString(R.string.total_games_played, scoreItems.size + 1)
-        tVAverageChange.text = "Average (difference): $average2 ($dif)"
-        tvTotalToday.text = "Total today: $dailyTotal"
+        tVGames.text = (scoreItems.size + 1).toString()
+        tVAverageChange.text = "$average2 ($dif)"
+        tvTotalToday.text = dailyTotal.toString()
+        tvAvgDifToday.text = difToday.toString()
 
         builder.setView(view2)
-        builder.setTitle("Game stats")
+        builder.setTitle(context.getString(R.string.game_stats))
 
-        builder.setNegativeButton("Close") { dialog: DialogInterface?, id: Int -> }
+        builder.setNegativeButton(context.getString(R.string.close)) { dialog: DialogInterface?, id: Int -> }
         builder.show()
     }
 }
