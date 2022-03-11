deb7824292abbbda7331b454a7ba700918bd9475
Koen <koenhabets1@gmail.com>
2022-03-11 22:16:55 +0100

    Score view

--- app/src/main/java/nl/koenhabets/yahtzeescore/ScoresView.kt
+++ app/src/main/java/nl/koenhabets/yahtzeescore/ScoresView.kt
@@ -1,11 +1,20 @@
 package nl.koenhabets.yahtzeescore
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
-import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class ScoresView : View {
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+class ScoresView : ConstraintLayout {
 
+    constructor(ctx: Context): super(ctx) {
+    }
+
+    constructor(ctx: Context, attributeSet: AttributeSet) : super(ctx, attributeSet) {
+        inflate(context, R.layout.yahtzee_scoresheet, this)
+        setBackgroundColor(Color.TRANSPARENT)
+    }
+
+    constructor(ctx: Context, attributeSet: AttributeSet, defStyleAttr: Int): super(ctx, attributeSet, defStyleAttr) {
     }
 }
