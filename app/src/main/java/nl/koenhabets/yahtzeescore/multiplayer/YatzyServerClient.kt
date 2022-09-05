65248059e09b7fc9b7786ba7443bc1063c7ef45e
Koen <koenhabets1@gmail.com>
2022-09-05 22:00:48 +0200

    Split yatzyserver, auto connect to backup server

--- app/src/main/java/nl/koenhabets/yahtzeescore/multiplayer/YatzyServerClient.kt
+++ app/src/main/java/nl/koenhabets/yahtzeescore/multiplayer/YatzyServerClient.kt
@@ -1,12 +1,6 @@
 package nl.koenhabets.yahtzeescore.multiplayer
 
 import android.util.Log
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.*
-import io.ktor.websocket.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -26,29 +20,50 @@ import java.util.*
 
 class YatzyServerClient(private val userId: String, private val userKey: String, private val clientVersion: Int) {
     private val tag = "YatzyServerClient"
+    private val host = "yahtzee.koenhabets.nl"
+    private val hostBackup = "yatzy-backup.koenhabets.nl"
+    private var yatzyServerWs: YatzyServerWs? = null
+    private var yatzyServerWsBackup: YatzyServerWs? = null
     private var listener: YatzyClientListener? = null
-    val client: HttpClient
-    private val scope: CoroutineScope
-    var webSocketSession: DefaultClientWebSocketSession? = null
-    var loggedIn = false
     val subscriptions = ArrayList<String>()
     private var lastScore: Message.Score? = null
     var username: String? = null
     var game: String? = null
-    var connecting = false
-    var reconnectTimer: Timer? = null
     var lastGameEnd = 0L
+    var wsFailCount = 0
+    var loggedIn = false
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+    var yatzyServerWsListener: YatzyServerWs.YatzyServerWsListener? = null
 
     init {
-        client = HttpClient(OkHttp) {
-            install(WebSockets) {
-                contentConverter = KotlinxWebsocketSerializationConverter((Json {
-                    ignoreUnknownKeys = true
-                }))
+        yatzyServerWsListener = (object : YatzyServerWs.YatzyServerWsListener {
+            override fun onLoggedIn() {
+                loggedIn = true
+                scope.launch {
+                    sendSubscribe(subscriptions)
+                    lastScore?.let {
+                        sendScore(it)
+                    }
+                }
             }
-        }
-        scope = CoroutineScope(Dispatchers.IO)
-        connect()
+
+            override fun onError() {
+                wsFailCount++
+                if (wsFailCount > 2 && yatzyServerWsBackup == null) {
+                    yatzyServerWsBackup =
+                        YatzyServerWs(hostBackup, userId, userKey, clientVersion, scope)
+                    yatzyServerWsBackup?.setYatzyServerWsListener(yatzyServerWsListener!!)
+                }
+            }
+
+            override fun onMessage(response: Response) {
+                scope.launch {
+                    processResponse(response)
+                }
+            }
+        })
+        yatzyServerWs = YatzyServerWs(host, userId, userKey, clientVersion, scope)
+        yatzyServerWs?.setYatzyServerWsListener(yatzyServerWsListener!!)
     }
 
     interface YatzyClientListener {
@@ -57,67 +72,6 @@ class YatzyServerClient(private val userId: String, private val userKey: String,
 
     fun setYatzyClientListener(listener: YatzyClientListener) {
         this.listener = listener
-    }
-
-    fun connect() {
-        scope.launch {
-            startWebsocket()
-        }
-
-        reconnectTimer = Timer()
-        reconnectTimer?.scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                scope.launch {
-                    if ((webSocketSession == null || !loggedIn) && !connecting) {
-                        webSocketSession?.close()
-                        startWebsocket()
-                    }
-                }
-            }
-        }, 5000, 5000)
-    }
-
-    fun disconnect() {
-        Log.i(tag, "Disconnecting")
-        scope.launch {
-            webSocketSession?.close()
-        }
-        reconnectTimer?.cancel()
-    }
-
-    private suspend fun startWebsocket() {
-        try {
-            connecting = true
-            client.wss(
-                method = HttpMethod.Get,
-                host = "yahtzee.koenhabets.nl",
-                port = 443,
-                path = "/api/v1/ws"
-            ) {
-                webSocketSession = this
-                login()
-                connecting = false
-                while (true) {
-                    val message = incoming.receive() as? Frame.Text
-                    if (message !== null) {
-                        try {
-                            val res = Json.decodeFromString<Response>(message.readText())
-                            Log.i(tag, res.toString())
-                            processResponse(res)
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            Log.i(tag, "Disconnected")
-            webSocketSession = null
-            loggedIn = false
-            connecting = false
-        }
     }
 
     fun endGame(game: String, versionString: String, versionCode: Int) {
@@ -135,7 +89,7 @@ class YatzyServerClient(private val userId: String, private val userKey: String,
         val endAction = Message.EndGame(game, versionString, versionCode)
         val message =
             Message(ActionType.endGame, Json.encodeToJsonElement(endAction).jsonObject)
-        webSocketSession?.sendSerialized(message)
+        sendMessage(message)
     }
 
     fun subscribe(userId: String) {
@@ -174,7 +128,7 @@ class YatzyServerClient(private val userId: String, private val userKey: String,
         Log.i(tag, "Sending score ${score.score}")
         val message =
             Message(ActionType.score, Json.encodeToJsonElement(score).jsonObject)
-        webSocketSession?.sendSerialized(message)
+        sendMessage(message)
     }
 
     private suspend fun sendSubscribe(userIds: ArrayList<String>) {
@@ -186,27 +140,18 @@ class YatzyServerClient(private val userId: String, private val userKey: String,
                     ActionType.subscribe,
                     Json.encodeToJsonElement(subscribeAction).jsonObject
                 )
-            webSocketSession?.sendSerialized(message)
+            sendMessage(message)
         }
 
     }
 
+    private suspend fun sendMessage(message: Message) {
+        yatzyServerWs?.sendMessage(message)
+        yatzyServerWsBackup?.sendMessage(message)
+    }
+
     private suspend fun processResponse(response: Response) {
-        if (response.response === ResponseType.loginResponse) {
-            val resData = Json.decodeFromJsonElement<Response.LoginResponse>(response.data)
-            if (resData.success) {
-                Log.i(tag, "Logged in")
-                loggedIn = true
-                scope.launch {
-                    sendSubscribe(subscriptions)
-                    lastScore?.let {
-                        sendScore(it)
-                    }
-                }
-            } else {
-                Log.e(tag, "Login error")
-            }
-        } else if (response.response === ResponseType.scoreResponse) {
+        if (response.response === ResponseType.scoreResponse) {
             val resData = Json.decodeFromJsonElement<Response.ScoreResponse>(response.data)
             withContext(Dispatchers.Main) {
                 listener?.onScore(resData)
@@ -217,12 +162,10 @@ class YatzyServerClient(private val userId: String, private val userKey: String,
         }
     }
 
-    private suspend fun login() {
-        Log.i(tag, "Start login $userId")
-        val loginAction = Message.Login(userId, userKey, clientVersion)
-        val message =
-            Message(ActionType.login, Json.encodeToJsonElement(loginAction).jsonObject)
-        webSocketSession?.sendSerialized(message)
+    fun disconnect() {
+        yatzyServerWs?.disconnect()
+        yatzyServerWsBackup?.disconnect()
+
     }
 
     companion object { // todo this will be moved when Multiplayer.java is rewritten
